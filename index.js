68f7818421d54138bc2abea6c0e6bc1192300ad5
Saad Tajwar <saads@ad.unc.edu>
2023-06-18 00:41:54 -0400

    Bruh

--- index.js
+++ index.js
@@ -2,56 +2,79 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const Note = require('./models/note')
+app.use(express.static('build'))
 app.use(express.json())
 const cors = require('cors')
 app.use(cors())
-app.use(express.static('build'))
 const mongoose = require('mongoose')
 
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello world!</h1>')
-  })
+app.get('/', (request, response) => {
+  response.send('<h1>Hello world!</h1>')
+})
 
-  app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-      response.json(notes)
-    })
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
   })
+})
 
-  app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id).then(note => {
+    if (note) {
       response.json(note)
-    })
-  })
+    } else {
+      response.status(404).end()
+    }
+  }).catch(error => next(error))
+})
 
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id).then(result => {
     response.status(204).end()
-  })
+  }).catch(error => next(error))
+})
 
-  const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-    return maxId + 1
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({error: 'content missing'})
   }
 
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
-
-    if (!body.content) {
-      return response.status(400).json({error: 'content missing'})
-    }
-
-    const note = new Note({
-      content: body.content,
-      important: body.important || false
-    })
-
-    note.save().then(savedNote => {
-      response.json(savedNote)
-    })
+  const note = new Note({
+    content: body.content,
+    important: body.important || false
   })
-  
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true }).then(updatedNote => {
+    response.json(updatedNote)
+  }).catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
