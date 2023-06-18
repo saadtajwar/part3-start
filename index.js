434719913d6657e8983fbcd299305f425615b613
Saad Tajwar <saads@ad.unc.edu>
2023-06-18 09:58:40 -0400

    Ready to build

--- index.js
+++ index.js
@@ -34,12 +34,8 @@ app.delete('/api/notes/:id', (request, response, next) => {
   }).catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({error: 'content missing'})
-  }
 
   const note = new Note({
     content: body.content,
@@ -48,17 +44,13 @@ app.post('/api/notes', (request, response) => {
 
   note.save().then(savedNote => {
     response.json(savedNote)
-  })
+  }).catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
-  const note = {
-    content: body.content,
-    important: body.important
-  }
+  const {content, important} = request.body
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true }).then(updatedNote => {
+  Note.findByIdAndUpdate(request.params.id, {content, important}, { new: true, runValidators: true, content: 'query' }).then(updatedNote => {
     response.json(updatedNote)
   }).catch(error => next(error))
 })
@@ -68,7 +60,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
+  }
 
   next(error)
 }
